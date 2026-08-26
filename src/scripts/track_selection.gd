87653f782cb3d6d89cc8609831f55ee9ddee1c58
lsalaun <loik.salaun@gmail.com>
2026-08-26 17:10:17 +0200

    Add track 02

--- src/scripts/track_selection.gd
+++ src/scripts/track_selection.gd
@@ -3,10 +3,12 @@ extends Node
 ## Autoload: holds the track picked from CircuitMenu so `main.tscn` knows
 ## which track scene to instantiate on launch. Names match the circuits
 ## src/wipeout/game.c's def.circuits associates with these PSX tracks
-## (TRACK01 -> TERRAMAX, TRACK12 -> KORODERA, venom class paths).
+## (TRACK01 -> TERRAMAX, TRACK02 -> ALTIMA VII, TRACK12 -> KORODERA, venom
+## class paths).
 
 const TRACKS: Array[Dictionary] = [
 	{"name": "TERRAMAX", "scene": "res://scenes/Track01.tscn"},
+	{"name": "ALTIMA VII", "scene": "res://scenes/Track02.tscn"},
 	{"name": "KORODERA", "scene": "res://scenes/Track12.tscn"},
 ]
 

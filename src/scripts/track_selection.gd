705bda247b21f3c4973d0f2a91dd95d2801d5670
lsalaun <loik.salaun@gmail.com>
2026-08-28 13:36:15 +0200

    Ajout des tracks 4, 5, 6 et 7

--- src/scripts/track_selection.gd
+++ src/scripts/track_selection.gd
@@ -4,13 +4,19 @@ extends Node
 ## which track scene to instantiate on launch. Names match the circuits
 ## src/wipeout/game.c's def.circuits associates with these PSX tracks
 ## (TRACK01 -> TERRAMAX venom, TRACK02 -> ALTIMA VII venom, TRACK03 ->
-## ALTIMA VII rapier, TRACK12 -> KORODERA venom). Optional `circuit` is the
-## def.circuits key used by RaceField when `name` is a class-specific label.
+## ALTIMA VII rapier, TRACK04 -> KARBONIS V venom, TRACK05 -> KARBONIS V
+## rapier, TRACK06 -> TERRAMAX rapier, TRACK07 -> KORODERA rapier,
+## TRACK12 -> KORODERA venom). Optional `circuit` is the def.circuits key
+## used by RaceField when `name` is a class-specific label.
 
 const TRACKS: Array[Dictionary] = [
 	{"name": "TERRAMAX", "scene": "res://scenes/Track01.tscn"},
 	{"name": "ALTIMA VII", "scene": "res://scenes/Track02.tscn"},
 	{"name": "ALTIMA VII RAPIER", "scene": "res://scenes/Track03.tscn", "circuit": "ALTIMA VII"},
+	{"name": "KARBONIS V", "scene": "res://scenes/Track04.tscn"},
+	{"name": "KARBONIS V RAPIER", "scene": "res://scenes/Track05.tscn", "circuit": "KARBONIS V"},
+	{"name": "TERRAMAX RAPIER", "scene": "res://scenes/Track06.tscn", "circuit": "TERRAMAX"},
+	{"name": "KORODERA RAPIER", "scene": "res://scenes/Track07.tscn", "circuit": "KORODERA"},
 	{"name": "KORODERA", "scene": "res://scenes/Track12.tscn"},
 ]
 
